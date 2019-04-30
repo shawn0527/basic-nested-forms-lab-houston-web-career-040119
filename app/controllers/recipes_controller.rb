f9bc0351adb12b7168cca3d40c82da97b176a250
Shawn Ren <shawnren527@gmail.com>
2019-04-30 19:54:26 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,7 +14,7 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.create (recipe_params)
+    @recipe = Recipe.create(recipe_params)
     redirect_to @recipe
   end
 
